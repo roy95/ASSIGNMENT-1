f6ac0be8faa7e514b9522d412f907d83df9e896d
roy95 <arkadeb555roy@gmail.com>
2025-08-24 11:41:59 +0530

    Add files via upload

---
+++ Simple_programme.py
@@ -0,0 +1,14 @@
+i=input("enter first no")
+i=float(i)
+r=input("enter second no")
+r=float(r)
+a=""
+a=input("Enter Your First name")
+b=""
+b=input("Enter Your Second name")
+print("Your sum is", i+r)
+print("Your subtraction is", i-r)
+print("Your multiplication is", i*r)
+print("Your divide is", i/r)
+print("hello", (a+b)+"!","Welcome to the python programme")
+
